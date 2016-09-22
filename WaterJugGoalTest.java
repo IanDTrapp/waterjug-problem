6b7e5b9a2fb9783b6d444d049c79c077b444be03
Ian D Trapp <IanDTrapp@gmail.com>
2016-09-22 19:39:15 -0400

    Update WaterJugGoalTest.java

--- WaterJugGoalTest.java
+++ WaterJugGoalTest.java
@@ -2,25 +2,12 @@ package waterjug;
 
 import aima.core.search.framework.GoalTest;
 
-/**
- * @author Ravi Mohan
- * 
- */
 public class WaterJugGoalTest implements GoalTest {
-	int[] goal = new int[] {2,0};
-
+	WaterJugBoard goal = new WaterJugBoard(0,0);
+	
 	public boolean isGoalState(Object state) {
-		/*WaterJugBoard thisState =  (WaterJugBoard) state;
-		System.out.println("thisState: " + thisState);*/
-		int[] thisState = (int[]) state;
-		System.out.println("State:" + thisState[0] + " " + thisState[1]);
-		System.out.flush();
-		if(thisState[0] == goal[0] && thisState[1] == goal[1]) {
-			return true;
-		} else {
-			return false;
-		}
-		
-		//return thisState.equals(goal);
+		System.out.println(state);
+		WaterJugBoard jugs = (WaterJugBoard) state;
+		return jugs.equals(goal);
 	}
 }
