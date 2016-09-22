289a0ddcfa0e8fbfb32fe0fe6641483482fb1fe5
Ian D Trapp <IanDTrapp@gmail.com>
2016-09-22 19:40:29 -0400

    Update WaterJugBoard.java

--- WaterJugBoard.java
+++ WaterJugBoard.java
@@ -11,53 +11,93 @@ public class WaterJugBoard {
 	public static Action FILL_2 = new DynamicAction("fill2");
 	public static Action EMPTY_TO_1 = new DynamicAction("emptyTo1");
 	public static Action EMPTY_TO_2 = new DynamicAction("emptyTo2");
+	private static int[] state;
+	
+	static int JUG_1_MAX = 4;
+	static int JUG_2_MAX = 3;
 	
 	public WaterJugBoard() {
+		state = new int[] {0,0};
+	}
+	
+	public WaterJugBoard(int a, int b) {
+		state = new int[] {0,0};
 		
+		setJug1Val(a);
+		setJug2Val(b);
+	}
+	
+	public int[] getState() {
+		return state;
+	}
+	
+	public static int getJug1Val() {
+		return state[0];
+	}
+	
+	public static int getJug2Val() {
+		return state[1];
+	}
+	
+	public static void setJug1Val(int val) {
+		state[0] = val;
+	}
+	
+	public static void setJug2Val(int val) {
+		state[1] = val;
 	}
 
-	public static int[] fill1(int[] s) {
+	public void fill1() {
 		System.out.println("Fill 1");
-		s[0] = 4;
-		return s;
+		setJug1Val(4);
 	}
 
-	public static int[] fill2(int[] s) {
+	public void fill2() {
 		System.out.println("Fill 2");
-		s[1] = 3;
-		return s;
+		setJug2Val(3);
 	}
 
-	public static int[] empty1(int[] s) {
+	public void empty1() {
 		System.out.println("Empty 1");
-		s[0] = 0;
-		return s;
+		setJug1Val(0);
 	}
 
-	public static int[] empty2(int[] s) {
+	public void empty2() {
 		System.out.println("Empty 2");
-		s[1] = 0;
-		return s;
+		setJug2Val(0);
 	}
 
-	public static int[] emptyTo1(int[] s) {
+	public void emptyTo1() {
 		System.out.println("Empty to 1");
-		while(s[0] < 4 && s[1] > 0) {
-			s[0]++;
-			s[1]--;
+		if(getJug2Val() > JUG_1_MAX - getJug1Val()) {
+			setJug2Val(getJug2Val() - (JUG_1_MAX - getJug1Val()));
+			setJug1Val(JUG_1_MAX);
 		}
-		return s;
-	}
-
-	public static int[] emptyTo2(int[] s) {
-		System.out.println("Empty to 2");
-		while(s[1] < 3 && s[0] > 0) {
-			s[1]++;
-			s[0]--;
+		else {
+			setJug1Val(getJug1Val() + getJug2Val());
+			setJug2Val(0);
 		}
 		
-		//System.out.println("s: " + s);
-		return s;
 	}
 
+	public void emptyTo2() {
+		System.out.println("Empty to 2");
+		if(getJug1Val() > JUG_2_MAX - getJug2Val()) {
+			setJug1Val(getJug1Val() - (JUG_2_MAX - getJug2Val()));
+			setJug2Val(JUG_2_MAX);
+		} else {
+			setJug2Val(getJug2Val() + getJug1Val());
+			setJug1Val(0);
+		}
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		System.out.println("Equals function: " + o);
+		if(this == o) {
+			return true;
+		}
+		
+		return false;
+	}
 }
